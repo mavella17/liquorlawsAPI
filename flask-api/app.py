b0f70ddb095a0246e942d0205523799db645a77d
Matthew Avella <mavella@gatech.edu>
2023-07-10 18:42:41 +0000

    Initial Commit

---
+++ flask-api/app.py
@@ -0,0 +1,81 @@
+import git
+from flask import Flask, jsonify, render_template, url_for, flash, redirect, request
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', subtitle='Home Page',
+                           text='This is the home page')
+
+@app.route('/states', methods=['GET'])
+def get_states():
+    # Logic to retrieve and format the list of states
+    states = {
+    'AK': 'Alaska',
+    'AL': 'Alabama',
+    'AR': 'Arkansas',
+    'AZ': 'Arizona',
+    'CA': 'California',
+    'CO': 'Colorado',
+    'CT': 'Connecticut',
+    'DC': 'District of Columbia',
+    'DE': 'Delaware',
+    'FL': 'Florida',
+    'GA': 'Georgia',
+    'HI': 'Hawaii',
+    'IA': 'Iowa',
+    'ID': 'Idaho',
+    'IL': 'Illinois',
+    'IN': 'Indiana',
+    'KS': 'Kansas',
+    'KY': 'Kentucky',
+    'LA': 'Louisiana',
+    'MA': 'Massachusetts',
+    'MD': 'Maryland',
+    'ME': 'Maine',
+    'MI': 'Michigan',
+    'MN': 'Minnesota',
+    'MO': 'Missouri',
+    'MS': 'Mississippi',
+    'MT': 'Montana',
+    'NC': 'North Carolina',
+    'ND': 'North Dakota',
+    'NE': 'Nebraska',
+    'NH': 'New Hampshire',
+    'NJ': 'New Jersey',
+    'NM': 'New Mexico',
+    'NV': 'Nevada',
+    'NY': 'New York',
+    'OH': 'Ohio',
+    'OK': 'Oklahoma',
+    'OR': 'Oregon',
+    'PA': 'Pennsylvania',
+    'RI': 'Rhode Island',
+    'SC': 'South Carolina',
+    'SD': 'South Dakota',
+    'TN': 'Tennessee',
+    'TX': 'Texas',
+    'UT': 'Utah',
+    'VA': 'Virginia',
+    'VT': 'Vermont',
+    'WA': 'Washington',
+    'WI': 'Wisconsin',
+    'WV': 'West Virginia',
+    'WY': 'Wyoming'
+}
+    return jsonify(states)
+
+
+@app.route('/states/<state_code>', methods=['GET'])
+def get_state_liquor_laws(state_code):
+    # Logic to retrieve and format liquor laws for the given state
+    liquor_laws = {
+        'state_code': state_code,
+        #'laws': 'Liquor laws for {}'.format(state_code)
+    }
+    return jsonify(liquor_laws)
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
