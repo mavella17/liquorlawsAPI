1b49a962319ca7da2caad37ea8e9b78ae1e4d5b0
Matthew Avella <mavella@gatech.edu>
2023-07-12 17:25:12 +0000

    style fixes, added checkstyle

--- flask-api/app.py
+++ flask-api/app.py
@@ -1,5 +1,6 @@
 import git
-from flask import Flask, jsonify, render_template, url_for, flash, redirect, request
+from flask import Flask, jsonify, render_template
+from flask import url_for, flash, redirect, request
 import pandas as pd
 import sqlalchemy as db
 from sqlalchemy import select
@@ -8,78 +9,80 @@ app = Flask(__name__)
 engine = db.create_engine('sqlite:///laws.db')
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
+
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html', subtitle='Home Page',
                            text='This is the home page')
 
+
 # gets a list of all laws
-@app.route('/laws', methods =['GET'])
+@app.route('/laws', methods=['GET'])
 def laws():
     sql = """Select * from laws;"""
     df = pd.read_sql(sql, con=engine)
     results = df.to_dict('records')
     return jsonify(results)
 
+
 # gets a list of all states
 @app.route('/laws/states.json', methods=['GET'])
 def get_states():
     # Logic to retrieve and format the list of states
-    states = {
-    'AK': 'Alaska',
-    'AL': 'Alabama',
-    'AR': 'Arkansas',
-    'AZ': 'Arizona',
-    'CA': 'California',
-    'CO': 'Colorado',
-    'CT': 'Connecticut',
-    'DC': 'District of Columbia',
-    'DE': 'Delaware',
-    'FL': 'Florida',
-    'GA': 'Georgia',
-    'HI': 'Hawaii',
-    'IA': 'Iowa',
-    'ID': 'Idaho',
-    'IL': 'Illinois',
-    'IN': 'Indiana',
-    'KS': 'Kansas',
-    'KY': 'Kentucky',
-    'LA': 'Louisiana',
-    'MA': 'Massachusetts',
-    'MD': 'Maryland',
-    'ME': 'Maine',
-    'MI': 'Michigan',
-    'MN': 'Minnesota',
-    'MO': 'Missouri',
-    'MS': 'Mississippi',
-    'MT': 'Montana',
-    'NC': 'North Carolina',
-    'ND': 'North Dakota',
-    'NE': 'Nebraska',
-    'NH': 'New Hampshire',
-    'NJ': 'New Jersey',
-    'NM': 'New Mexico',
-    'NV': 'Nevada',
-    'NY': 'New York',
-    'OH': 'Ohio',
-    'OK': 'Oklahoma',
-    'OR': 'Oregon',
-    'PA': 'Pennsylvania',
-    'RI': 'Rhode Island',
-    'SC': 'South Carolina',
-    'SD': 'South Dakota',
-    'TN': 'Tennessee',
-    'TX': 'Texas',
-    'UT': 'Utah',
-    'VA': 'Virginia',
-    'VT': 'Vermont',
-    'WA': 'Washington',
-    'WI': 'Wisconsin',
-    'WV': 'West Virginia',
-    'WY': 'Wyoming',
-    'PR': 'Puerto Rico'
-}
+    states = {'AK': 'Alaska',
+              'AL': 'Alabama',
+              'AR': 'Arkansas',
+              'AZ': 'Arizona',
+              'CA': 'California',
+              'CO': 'Colorado',
+              'CT': 'Connecticut',
+              'DC': 'District of Columbia',
+              'DE': 'Delaware',
+              'FL': 'Florida',
+              'GA': 'Georgia',
+              'HI': 'Hawaii',
+              'IA': 'Iowa',
+              'ID': 'Idaho',
+              'IL': 'Illinois',
+              'IN': 'Indiana',
+              'KS': 'Kansas',
+              'KY': 'Kentucky',
+              'LA': 'Louisiana',
+              'MA': 'Massachusetts',
+              'MD': 'Maryland',
+              'ME': 'Maine',
+              'MI': 'Michigan',
+              'MN': 'Minnesota',
+              'MO': 'Missouri',
+              'MS': 'Mississippi',
+              'MT': 'Montana',
+              'NC': 'North Carolina',
+              'ND': 'North Dakota',
+              'NE': 'Nebraska',
+              'NH': 'New Hampshire',
+              'NJ': 'New Jersey',
+              'NM': 'New Mexico',
+              'NV': 'Nevada',
+              'NY': 'New York',
+              'OH': 'Ohio',
+              'OK': 'Oklahoma',
+              'OR': 'Oregon',
+              'PA': 'Pennsylvania',
+              'RI': 'Rhode Island',
+              'SC': 'South Carolina',
+              'SD': 'South Dakota',
+              'TN': 'Tennessee',
+              'TX': 'Texas',
+              'UT': 'Utah',
+              'VA': 'Virginia',
+              'VT': 'Vermont',
+              'WA': 'Washington',
+              'WI': 'Wisconsin',
+              'WV': 'West Virginia',
+              'WY': 'Wyoming',
+              'PR': 'Puerto Rico'
+              }
     return jsonify(states)
 
 
@@ -107,24 +110,26 @@ def get_state_liquor_laws(state_code):
 
 
 # gets every states ages
-@app.route('/laws/age.json', methods =['GET'])
+@app.route('/laws/age.json', methods=['GET'])
 def age():
-    sql = """Select Abbreviation, "Age: Consumption", "Age: Purchasing" from laws;"""
+    sql = """Select Abbreviation, "Age: Consumption", """
+    sql += """"Age: Purchasing" from laws;"""
     df = pd.read_sql(sql, con=engine)
     results = df.to_dict('records')
     return jsonify(results)
 
 
 # returns all notes from all states
-@app.route('/laws/notes.json', methods =['GET'])
+@app.route('/laws/notes.json', methods=['GET'])
 def notes():
     sql = "Select Abbreviation, Location, Notes from laws;"
     df = pd.read_sql(sql, con=engine)
     results = df.to_dict('records')
     return jsonify(results)
 
+
 # grocery sale laws for every state
-@app.route('/laws/grocerysales.json', methods =['GET'])
+@app.route('/laws/grocerysales.json', methods=['GET'])
 def grocerysales():
     sql = """Select Abbreviation, Location, "Grocery store sales: Beer",
                "Grocery store sales: Wine",
@@ -133,25 +138,29 @@ def grocerysales():
     results = df.to_dict('records')
     return jsonify(results)
 
+
 # sale hours for every state
-@app.route('/laws/salehours.json', methods =['GET'])
+@app.route('/laws/salehours.json', methods=['GET'])
 def salehours():
     sql = """Select Abbreviation, Location, "Alcohol sale hours: On-premises",
                "Alcohol sale hours: Off-premises" from laws;"""
     df = pd.read_sql(sql, con=engine)
-    
+
     results = df.to_dict('records')
     return jsonify(results)
 
+
 # alcohol beverage controlled laws for every state
-@app.route('/laws/abc.json', methods =['GET'])
+@app.route('/laws/abc.json', methods=['GET'])
 def abc():
     sql = """Select Abbreviation, Location, "Alcoholic beverage control state: Beer",
                "Alcoholic beverage control state: Wine",
-               "Alcoholic beverage control state: Distilled spirits" from laws;"""
+               "Alcoholic beverage control state: Distilled spirits"
+                from laws;"""
     df = pd.read_sql(sql, con=engine)
     results = df.to_dict('records')
     return jsonify(results)
+
 
 # used with webhooks to update server
 @app.route("/update_server", methods=['POST'])
@@ -163,6 +172,7 @@ def webhook():
         return 'Updated PythonAnywhere successfully', 200
     else:
         return 'Wrong event type', 400
+
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
